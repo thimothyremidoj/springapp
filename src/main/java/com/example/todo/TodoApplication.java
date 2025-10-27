7fb8a1b3470d431be950ad64d0d92a2ecb395921
ThimothyRemido J <727723euai128@skcet.ac.in>
2025-10-27 22:21:31 +0530

    second message

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -3,7 +3,6 @@ package com.example.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration.class})
 @EnableJpaRepositories
@@ -12,6 +11,7 @@ public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
